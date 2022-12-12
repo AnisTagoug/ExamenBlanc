a333b8787f7736e28602fd66a69b7322dad7518c
Tagoug anis <60471739+DonSopra@users.noreply.github.com>
2022-12-12 16:00:09 +0100

    Delete Desktop/Tp1 directory

--- Desktop/Tp1/src/main/java/com/example/demo/Repository/CategorieProduitRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Entity.CategorieProduit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategorieProduitRepository extends JpaRepository<CategorieProduit,Long>{
-}
--- Desktop/Tp1/src/main/java/com/example/demo/Repository/ProduitRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Repository;
-
-
-import com.example.demo.Entity.Produit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProduitRepository extends JpaRepository<Produit,Long>{
-}
